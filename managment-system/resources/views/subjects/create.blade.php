9ede97a2791c32be7f77c97f0f7f51b69e3124f3
angelov2000@abv.bg <angelov2000@abv.bg>
2022-05-31 22:23:21 +0300

    subjects-views

--- managment-system/resources/views/subjects/create.blade.php
+++ managment-system/resources/views/subjects/create.blade.php
@@ -12,11 +12,15 @@
             <label for="" class="form-label">Description</label>
             <input type="text" class="form-control form-control-sm" name="description">
         </div>
-        <select class="form-select" multiple aria-label="multiple select example" name="course_id[]">
-          @foreach ($courses as $item)
-            <option value="{{$item->id}}">{{$item->title}}</option>
-          @endforeach
-        </select>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="mb-3">
+            <select class="form-select" multiple aria-label="multiple select example" name="course_id[]">
+                @foreach ($courses as $item)
+                    <option value="{{ $item->id }}">{{ $item->title }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+        </div>
     </form>
 @endsection
--- managment-system/resources/views/subjects/edit.blade.php
+++ managment-system/resources/views/subjects/edit.blade.php
@@ -18,6 +18,6 @@
                     {{ $item->title }}</option>
             @endforeach
         </select>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary mt-2">Submit</button>
     </form>
 @endsection
